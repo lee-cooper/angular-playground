7e4f9c0b4c31d1a2fcad9d6b8b3de9da2514dd1e
Lee Cooper <cooperlee372@gmail.com>
2022-07-13 21:04:38 -0400

    add angular material. learn multi slot projection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgContentComponent } from './components/ng-content/ng-content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'ng-content',
+    component: NgContentComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { NgContentComponent } from './components/ng-content/ng-content.component
 import { SingleProjectionComponent } from './components/ng-content/single-projection/single-projection.component';
 import { MultiSlotProjectionComponent } from './components/ng-content/multi-slot-projection/multi-slot-projection.component';
 import { ConditionalProjectionComponent } from './components/ng-content/conditional-projection/conditional-projection.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +17,16 @@ import { ConditionalProjectionComponent } from './components/ng-content/conditio
     NgContentComponent,
     SingleProjectionComponent,
     MultiSlotProjectionComponent,
-    ConditionalProjectionComponent
+    ConditionalProjectionComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
