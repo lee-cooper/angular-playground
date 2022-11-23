f194ea652b26ba2d73d8e504894ba49d8c35ca86
Lee Cooper <cooperlee372@gmail.com>
2022-11-23 12:40:43 -0500

    update typescript version

--- src/app/directives/underline.directive.spec.ts
+++ src/app/directives/underline.directive.spec.ts
@@ -1,8 +1,11 @@
+import { ElementRef } from '@angular/core';
 import { UnderlineDirective } from './underline.directive';
+
+const mockElementRef = {} as ElementRef;
 
 describe('UnderlineDirective', () => {
   it('should create an instance', () => {
-    const directive = new UnderlineDirective();
+    const directive = new UnderlineDirective(mockElementRef);
     expect(directive).toBeTruthy();
   });
 });
