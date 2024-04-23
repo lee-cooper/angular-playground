dc6b2a162926975c611044e2e9b1094339da4cc7
Lee Cooper <cooperlee372@gmail.com>
2024-04-23 19:14:05 -0400

    drag and drop playing around

--- src/app/components/drag-and-drop/drag-and-drop.component.ts
+++ src/app/components/drag-and-drop/drag-and-drop.component.ts
@@ -5,28 +5,42 @@ import {
   CdkDropListGroup,
   moveItemInArray,
 } from '@angular/cdk/drag-drop';
-import { Component } from '@angular/core';
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import {
+  AfterViewInit,
+  Component,
+  OnDestroy,
+  ViewContainerRef,
+} from '@angular/core';
 import { User, data } from './user-data';
+
+// https://www.youtube.com/watch?v=ZfWCr8BzYHY
 
 @Component({
   selector: 'app-drag-and-drop',
   standalone: true,
   imports: [CdkDropListGroup, CdkDropList, CdkDrag],
-  template: `<div cdkDropListGroup class="users-container">
-    <div
-      cdkDropList
-      [cdkDropListData]="users"
-      class="users-list"
-      (cdkDropListDropped)="drop($event)"
-    >
-      @for (user of users; track user.id) {
-        <div cdkDrag class="user">{{ user.name }}</div>
-      }
+  template: ` <div class="example-boundary">
+      <div class="example-box" cdkDragBoundary=".mat-drawer-content" cdkDrag>
+        I can only be dragged within the parent container.
+      </div>
     </div>
-  </div>`,
+
+    <div cdkDropListGroup class="users-container">
+      <div
+        cdkDropList
+        [cdkDropListData]="users"
+        class="users-list"
+        (cdkDropListDropped)="drop($event)"
+      >
+        @for (user of users; track user.id) {
+          <div cdkDrag class="user">{{ user.name }}</div>
+        }
+      </div>
+    </div>`,
   styleUrl: './drag-and-drop.component.scss',
 })
-export class DragAndDropComponent {
+export class DragAndDropComponent implements AfterViewInit, OnDestroy {
   users: User[] = data;
 
   drop(dropEvent: CdkDragDrop<User[]>) {
@@ -35,5 +49,30 @@ export class DragAndDropComponent {
       dropEvent.previousIndex,
       dropEvent.currentIndex,
     );
+  }
+
+  private _overlayRef: OverlayRef;
+
+  constructor(
+    private _overlay: Overlay,
+    private _viewContainerRef: ViewContainerRef,
+  ) {
+    this._overlayRef = this._overlay.create({});
+  }
+
+  ngAfterViewInit() {
+    this._overlayRef = this._overlay.create({
+      positionStrategy: this._overlay
+        .position()
+        .global()
+        .centerHorizontally()
+        .centerVertically(),
+      hasBackdrop: true,
+    });
+    this._overlayRef.backdropClick().subscribe(() => this._overlayRef.detach());
+  }
+
+  ngOnDestroy() {
+    this._overlayRef.dispose();
   }
 }
