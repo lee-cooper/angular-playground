81c3195fae38d7c21b6f2b3277dc31723ad6576d
Lee Cooper <20045743+lee-cooper@users.noreply.github.com>
2024-07-15 20:51:01 -0400

    last of ng18 upgrade

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-navigation',
-    templateUrl: './navigation.component.html',
-    styleUrls: ['./navigation.component.scss'],
-    standalone: true,
-    imports: [RouterLink]
+  selector: 'app-navigation',
+  template: `<nav class="nav ">
+    <a routerLink="/input-output" class="link">input-output</a>
+    <a routerLink="/ng-content" class="link">ng-content</a>
+    <a routerLink="/reactive-forms" class="link">reactive-forms</a>
+    <a routerLink="/drag-and-drop" class="link">drag-and-drop</a>
+  </nav>`,
+  styles: `
+    .nav {
+      width: 10rem;
+      max-width: 10rem;
+
+      .link {
+        width: 100%;
+        display: inline-block;
+        text-align: center;
+        padding: 1rem 0;
+        border: 1px solid lightgray;
+        border-style: inset;
+      }
+    }
+  `,
+  standalone: true,
+  imports: [RouterLink],
 })
-export class NavigationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class NavigationComponent {}
